cd75206e96f9edc3ec9ad434a2ee2dae4d849f67
balckowl <shiggyjr1492@gmail.com>
2025-04-17 23:09:37 +0900

    fix: cookie devided

--- src/app/enter/callback/welcome/page.tsx
+++ src/app/enter/callback/welcome/page.tsx
@@ -1,28 +1,31 @@
 import { auth } from "@/auth";
 import WelocomeWrapper from "@/components/not-musium/welocomeWrapper";
-// import { env } from "@/env.mjs";
-import { headers } from "next/headers";
+import { env } from "@/env.mjs";
+import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
+
+  const cookieHeader = cookies().toString();
+
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: await headers()
   });
 
   if (!session || !session.user?.id) {
     redirect("/");
   }
 
-  // const res = await fetch(`${env.NEXT_PUBLIC_APP_URL}/api/me/username`, {
-  //   method: "GET",
-  //   headers: await headers(),
-  // });
+  const res = await fetch(`${env.NEXT_PUBLIC_APP_URL}/api/me/username`, {
+    method: "GET",
+    headers: { cookie: cookieHeader },
+  });
 
-  // const { username } = await res.json();
+  const { username } = await res.json();
 
-  // if (username) {
-  //   redirect(`/${username}`);
-  // }
+  if (username) {
+    redirect(`/${username}`);
+  }
 
   return (
     <>
